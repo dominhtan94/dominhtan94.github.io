cbd95d405ff4a3d801da170d187f0bb35dfa49a8
Tấn Đỗ <dominhtan94@gmail.com>
2020-08-23 12:36:47 +0700

    update layout

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,16 +3,17 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "926bb7b0905168cfe35a9d79db26fcaf",
 "/": "926bb7b0905168cfe35a9d79db26fcaf",
-"main.dart.js": "0ac9429711deb69e4ee6eea93a26a572",
+"main.dart.js": "84d9edf2eec9b3501d9c9dc005f0acbf",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "f8e03a3c1844b7ffd9805bb716c63ca3",
-"assets/LICENSE": "a621b056869040941e658f9d5c7a1d48",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/LICENSE": "ad22a5d797206c5e7026e152c711f417",
+"assets/AssetManifest.json": "23a9b88d8df6bdac928f58464374f8f1",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/assets/mita.jpg": "35b93e63e622b330757a37b171e96c79"
 };
 
 self.addEventListener('activate', function (event) {
